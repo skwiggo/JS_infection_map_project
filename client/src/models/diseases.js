1b6e392228686a9ff23d5990896e01dd0ea5c471
skwiggo123@gmail.com <user@CODEBASE054.local>
2016-10-07 14:53:19 +0100

    added more facts

--- client/src/models/diseases.js
+++ client/src/models/diseases.js
@@ -4,13 +4,18 @@ var Fact = require('./fact');
 var Diseases = function() {
 
   var tbFact1 = new Fact({
-    comment: "A quarter of all deaths in Europe are estimated to have been caused by tuberculosis infection during the 19th and early 20th century.",
-    image: "http://www.ancient-origins.net/sites/default/files/styles/large/public/First-and-Last-Communion.jpg?itok=9hHKG13z"
+    comment: "The evidence for tuberculosis can be traced back to mummies in Ancient Egypt.",
+    image: "http://i.bnet.com/blogs/mummy.jpg"
   });
 
   var tbFact2 = new Fact({
     comment: "A quarter of all deaths in Europe are estimated to have been caused by tuberculosis infection during the 19th and early 20th century.",
     image: "http://www.ancient-origins.net/sites/default/files/styles/large/public/First-and-Last-Communion.jpg?itok=9hHKG13z"
+  });
+
+  var tbFact3 = new Fact({
+    comment: "TB is the leading killer of people living with HIV.",
+    image: "https://pbs.twimg.com/profile_images/710836583235260417/WzC93R20.jpg"
   });
 
   var tuberculosis = new Disease({
@@ -66,6 +71,8 @@ var yellowFever = new Disease({
           });
 
 tuberculosis.addFact(tbFact1);
+tuberculosis.addFact(tbFact2);
+tuberculosis.addFact(tbFact3);
 
   return [tuberculosis, yellowFever];
 }
