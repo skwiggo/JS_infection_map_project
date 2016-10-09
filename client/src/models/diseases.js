a75d37c72e28ca9daa64d4a488c7f9106c514fcf
skwiggo123@gmail.com <user@CODEBASE054.local>
2016-10-09 11:31:46 +0100

    finished tb and smallpox api data

--- client/src/models/diseases.js
+++ client/src/models/diseases.js
@@ -378,49 +378,49 @@ var tuberculosis = new Disease({
                         name: "UK", 
                         coords: {lat: 55.378051, 
                                  lng: -3.435973},
-                        mortality: "high"             
+                        mortality: "none"             
                         },  
                           
                         { 
                           name: "France", 
                           coords: {lat: 46.227638, 
                                    lng: 2.213749},
-                          mortality: "high"             
+                          mortality: "none"             
                         },
 
                         {
                           name: "Spain", 
                           coords: {lat: 40.463667, 
                                   lng: -3.74922},
-                          mortality: "high"             
+                          mortality: "none"             
                         },
 
                          {
                            name: "USA", 
                            coords: {lat: 37.09024, 
                                     lng: -95.712891},
-                           mortality: "high"             
+                           mortality: "none"             
                          },
                          
                          {
                            name: "South Africa", 
                            coords: {lat: -30.559482, 
                                     lng: 22.937506},
-                           mortality: "high"             
+                           mortality: "medium"             
                          },
 
                          {
                            name: "DR Congo", 
                            coords: {lat: -2.930652, 
                                     lng: 23.285279},
-                           mortality: "high"
+                           mortality: "low"
                          },
 
                           {
                             name: "Russia", 
                             coords: {lat: 62.333724,  
                                      lng: 92.189755},
-                            mortality: "high"             
+                            mortality: "low"             
                           },
 
                           {
@@ -434,112 +434,112 @@ var tuberculosis = new Disease({
                           name: "Argentina", 
                           coords: {lat: -38.416097, 
                                    lng: -63.616672},
-                          mortality: "high"             
+                          mortality: "low"             
                         },
                         
                         {
                           name: "Brazil", 
                           coords: {lat: -14.235004, 
                                    lng:  -51.92528},
-                          mortality: "high"             
+                          mortality: "low"             
                         },
 
                         {
                           name: "Mexico", 
                           coords: {lat: 23.634501, 
                                    lng: -102.552784},
-                          mortality: "high"             
+                          mortality: "low"             
                         },
                          
                         {
                           name: "China", 
                           coords: {lat: 1.352083, 
                                    lng: 103.819836},
-                          mortality: "high"             
+                          mortality: "medium"             
                         }],
 
           presentDay: [{
                         name: "UK", 
                         coords: {lat: 55.378051, 
                                  lng: -3.435973},
-                        mortality: "high"             
+                        mortality: "none"             
                         },  
                         
                       { 
                         name: "France", 
                         coords: {lat: 46.227638, 
                                  lng: 2.213749},
-                        mortality: "high"             
+                        mortality: "none"             
                       },
 
                       {
                         name: "Spain", 
                         coords: {lat: 40.463667, 
                                 lng: -3.74922},
-                        mortality: "high"             
+                        mortality: "none"             
                       },
 
                        {
                          name: "USA", 
                          coords: {lat: 37.09024, 
                                   lng: -95.712891},
-                         mortality: "high"             
+                         mortality: "none"             
                        },
                        
                        {
                          name: "South Africa", 
                          coords: {lat: -30.559482, 
                                   lng: 22.937506},
-                         mortality: "high"             
+                         mortality: "medium"             
                        },
 
                        {
                          name: "DR Congo", 
                          coords: {lat: -2.930652, 
                                   lng: 23.285279},
-                         mortality: "high"
+                         mortality: "medium"
                        },
 
                         {
                           name: "Russia", 
                           coords: {lat: 62.333724,  
                                    lng: 92.189755},
-                          mortality: "high"             
+                          mortality: "low"             
                         },
 
                         {
                           name: "India", 
                           coords: {lat: 20.593684, 
                                   lng: 78.96288},
-                          mortality: "high"             
+                          mortality: "medium"             
                         },
 
                       {
                         name: "Argentina", 
                         coords: {lat: -38.416097, 
                                  lng: -63.616672},
-                        mortality: "high"             
+                        mortality: "none"             
                       },
                       
                       {
                         name: "Brazil", 
                         coords: {lat: -14.235004, 
                                  lng:  -51.92528},
-                        mortality: "high"             
+                        mortality: "none"             
                       },
 
                       {
                         name: "Mexico", 
                         coords: {lat: 23.634501, 
                                  lng: -102.552784},
-                        mortality: "high"             
+                        mortality: "none"             
                       },
                        
                       {
                         name: "China", 
                         coords: {lat: 1.352083, 
                                  lng: 103.819836},
-                        mortality: "high"             
+                        mortality: "none"             
                       }],
                     });
 
