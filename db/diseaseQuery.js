550f4744cd44a6a0a240f94ceab78ea5e4b54e73
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2016-10-09 15:22:48 +0100

    Added a .all() function to the query prototype

--- db/diseaseQuery.js
+++ db/diseaseQuery.js
@@ -3,3 +3,14 @@ var MongoClient = require('mongodb').MongoClient;
 var DiseaseQuery = function(){
   this.url = "mongodb://localhost:27017/diseases";
 }
+
+Disease.prototype = {
+  all: function(onQueryFinished){
+    MongoClient.connect(this.url, function(err, db){
+      var collection = db.collection("disease");
+      collection.find().toArray(function(err, docs){
+        onQueryFinnished(docs);
+      })
+    })
+  }
+}
