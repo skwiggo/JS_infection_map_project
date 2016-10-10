a5e30e442c8e56c0b8f7b4b5c13e807de0e9977c
skwiggo123@gmail.com <user@CODEBASE054.local>
2016-10-10 23:24:45 +0100

    sorted smallpox seeds file out - slider works with all 4 diseases and all 3 time periods

--- seeds/smallPoxSeeds.js
+++ seeds/smallPoxSeeds.js
@@ -84,7 +84,92 @@ var smallPoxSeeds =  {
         lng: 103.819836},
         mortality: "high"             
         }
-      ]  
+      ],
+        twentiethCentury: [{
+        name: "UK", 
+        coords: {lat: 55.378051, 
+            lng: -3.435973},
+        mortality: "none"             
+        },  
+
+        { 
+        name: "France", 
+        coords: {lat: 46.227638, 
+              lng: 2.213749},
+        mortality: "none"             
+        },
+
+        {
+        name: "Spain", 
+        coords: {lat: 40.463667, 
+             lng: -3.74922},
+        mortality: "none"             
+        },
+
+        {
+        name: "USA", 
+        coords: {lat: 37.09024, 
+               lng: -95.712891},
+        mortality: "none"             
+        },
+
+        {
+        name: "South Africa", 
+        coords: {lat: -30.559482, 
+               lng: 22.937506},
+        mortality: "low"             
+        },
+
+        {
+        name: "DR Congo", 
+        coords: {lat: -2.930652, 
+               lng: 23.285279},
+        mortality: "medium"
+        },
+
+        {
+        name: "Russia", 
+        coords: {lat: 62.333724,  
+              lng: 92.189755},
+        mortality: "low"             
+        },
+
+        {
+        name: "India", 
+        coords: {lat: 20.593684, 
+             lng: 78.96288},
+        mortality: "none"             
+        },
+
+        {
+        name: "Argentina", 
+        coords: {lat: -38.416097, 
+              lng: -63.616672},
+        mortality: "low"             
+        },
+
+        {
+        name: "Brazil", 
+        coords: {lat: -14.235004, 
+              lng:  -51.92528},
+        mortality: "low"             
+        },
+
+        {
+        name: "Mexico", 
+        coords: {lat: 23.634501, 
+              lng: -102.552784},
+        mortality: "low"             
+        },
+
+        {
+        name: "China", 
+        coords: {lat: 34.560541,  
+              lng: 102.994444},
+        mortality: "low"             
+        }],
+
+        presentDay: []
   }
 
 
