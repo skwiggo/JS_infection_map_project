c81b0c797b5773ebf78d1d1e8ecffc8e005db285
Gillian McCabe <gillianmccabe@outlook.com>
2016-10-10 11:02:29 +0100

    updated facts

--- client/src/models/diseases.js
+++ client/src/models/diseases.js
@@ -29,28 +29,28 @@ var Diseases = function() {
   });
 
   var spFact1 = new Fact({
-    comment: "Smallpox is transmitted through extended face-to-face contact, or direct contact with infected body fluids or contaminated objects.",
-    image: "https://i.ytimg.com/vi/yqUFy-t4MlQ/maxresdefault.jpg"
+    comment: "Smallpox is a relatively weak virus when it’s all by itself. If you were to spray smallpox into the air, 90% of the virus was sprayed into the year would die within 24 hours. Because of this weakness, insects and animals are not able to transmit the disease at all. Humans can only get smallpox from other humans who have an active infection.",
+    image: "https://www.thestar.com/content/dam/thestar/news/insight/2015/05/22/sneeze-politics-sorting-out-fact-from-sinus-fiction/sneezing.jpg.size.xxlarge.letterbox.jpg"
   });
 
   var spFact2 = new Fact({
-    comment: "Insects and animals do not transmit smallpox. If aerosolized smallpox were to be released, 90% of the virus would die within 24 hours.",
-    image: "https://i.ytimg.com/vi/yqUFy-t4MlQ/maxresdefault.jpg"
+    comment: "As smallpox has not been active wordwide for almost 40 years, the current world population has almost no inbuilt immunity to the disease. It is therefore feared that if smallpox were to be used as a biological weapon, it could have a devastating effect.",
+    image: "http://science.dodlive.mil/files/2013/11/Caution-Biowarfare.jpg"
   });
 
   var spFact3 = new Fact({
-    comment: "Smallpox is the only human based infection that has been completely eradicated from the world. The world has been smallpox free since 1979 after a widescale vaccination programme.",
-    image: "http://www.museumofhealthcare.ca/explore/exhibits/vaccinations/smallpox.html"
+    comment: "Smallpox is the only human based infection that has been completely eradicated from the world. The world has been smallpox free since 1979 after a widescale vaccination programme. ",
+    image: "https://upload.wikimedia.org/wikipedia/commons/4/41/Dryvax.jpg"
   });
 
   var spFact4 = new Fact({
-    comment: "Smallpox is the only human based infection that has been completely eradicated from the world. The world has been smallpox free since 1979 after a widescale vaccination programme.",
-    image: "http://www.museumofhealthcare.ca/explore/exhibits/vaccinations/smallpox.html"
+    comment: "The first signs and symptoms of smallpox usually appear 12 to 14 days after infection, although the incubation period can range from 7 to 17 days. During this time, an infected person may look and feel normal.",
+    image: "http://www.freeiconspng.com/uploads/thermometer-png-icon-4.png"
   });
 
   var spFact5 = new Fact({
-    comment: "Smallpox is the only human based infection that has been completely eradicated from the world. The world has been smallpox free since 1979 after a widescale vaccination programme.",
-    image: "http://www.museumofhealthcare.ca/explore/exhibits/vaccinations/smallpox.html"
+    comment: "For centuries, epidemics of smallpox affected people all over the globe. But in 1796, an English doctor named Edward Jenner discovered a way to protect people from getting smallpox, and his experiments eventually led to the development of the first smallpox vaccine.",
+    image: "http://blog.rsb.org.uk/wp-content/uploads/2014/09/jenner.jpg"
   });
 
   var zikaFact1 = new Fact({
