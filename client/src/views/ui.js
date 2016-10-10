fa310d316838931ad21518d1b9aa6c2e6db113fb
James Scott <jamessct@hotmail.co.uk>
2016-10-10 10:37:47 +0100

    done merge stuff

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -6,29 +6,21 @@ var UI = function() {
   var center = {lat: 0, lng: 150.644};
   this.diseases = new Diseases();
   var map = new Map(container, center, 1);
+
   // this.getDisease(this.diseases, map);
   this.selectDropdown(map);
 }
 
 UI.prototype = {
-<<<<<<< HEAD
   selectDropdown: function (map) {
     var select = document.querySelector('select');
-    
     select.onchange = function() {
       var value = (select.selectedIndex);
           console.log(value);
-
       this.handleSelectChanged(event, this.diseases, map, value, select);
-    }.bind(this)
-=======
-  createMarker: function(country, map, disease) {
-    map.addMarker(country, disease);
->>>>>>> de51f4984955c8c4bfbddfd49fb321787b45f629
+    }.bind(this)  
   },
   handleSelectChanged: function(event, diseases, map, value, select) {
-    // var disease = this.diseases[value].name;
-    // console.log(disease)
     var option = select.options[value].value;
     console.log(option);
     for(disease of diseases) {
@@ -39,8 +31,8 @@ UI.prototype = {
       }
     } 
   },
-  createMarker: function(coords, map) {
-    map.customMarker(coords);
+  createMarker: function(country, map, disease) {
+      map.addMarker(country, disease);
   },
   getDisease: function(disease, map) {
     console.log(disease);
@@ -57,7 +49,5 @@ UI.prototype = {
     }
   }
 }
-
-
 
 module.exports = UI;
