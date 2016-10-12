5f19c8629ebf3c2485afe07284e56181c3c9915a
Gillian McCabe <gillianmccabe@outlook.com>
2016-10-12 11:45:18 +0100

    updated zombie facts

--- client/src/models/facts/zombFacts.js
+++ client/src/models/facts/zombFacts.js
@@ -2,32 +2,32 @@ var zombFacts = function(){
   this.data = [
     {
       disease: "Zombies",
-      comment: "The Zika virus was first discovered in Ugandan monkeys in 1947. It spread to the South Pacific and caused an outbreak on Yap Island in Micronesia in 2007. The island’s population was too small for scientists to observe whether the virus caused side effects like birth defects or neurological damage.",
-      image: "http://www.esayap.com/images/Island1.jpg"
+      comment: "ZOMMMMBIESSSSS!!! RUNNNNNNNN!!!!!",
+      image: "http://image.dek-d.com/27/0283/4578/121404621"
     },
 
     {
       disease: "Zombies",
-      comment: "The Zika virus was first discovered in Ugandan monkeys in 1947. It spread to the South Pacific and caused an outbreak on Yap Island in Micronesia in 2007. The island’s population was too small for scientists to observe whether the virus caused side effects like birth defects or neurological damage.",
-      image: "http://www.esayap.com/images/Island1.jpg"
+      comment: "ZOMMMMBIESSSSS!!! RUNNNNNNNN!!!!!",
+      image: "http://image.dek-d.com/27/0283/4578/121404621"
     },
 
     {
       disease: "Zombies",
-      comment: "The Zika virus was first discovered in Ugandan monkeys in 1947. It spread to the South Pacific and caused an outbreak on Yap Island in Micronesia in 2007. The island’s population was too small for scientists to observe whether the virus caused side effects like birth defects or neurological damage.",
-      image: "http://www.esayap.com/images/Island1.jpg"
+      comment: "ZOMMMMBIESSSSS!!! RUNNNNNNNN!!!!!",
+      image: "http://image.dek-d.com/27/0283/4578/121404621"
     },
 
     {
       disease: "Zombies",
-      comment: "The Zika virus was first discovered in Ugandan monkeys in 1947. It spread to the South Pacific and caused an outbreak on Yap Island in Micronesia in 2007. The island’s population was too small for scientists to observe whether the virus caused side effects like birth defects or neurological damage.",
-      image: "http://www.esayap.com/images/Island1.jpg"
+      comment: "ZOMMMMBIESSSSS!!! RUNNNNNNNN!!!!!",
+      image: "http://image.dek-d.com/27/0283/4578/121404621"
     },
 
     {
       disease: "Zombies",
-      comment: "The Zika virus was first discovered in Ugandan monkeys in 1947. It spread to the South Pacific and caused an outbreak on Yap Island in Micronesia in 2007. The island’s population was too small for scientists to observe whether the virus caused side effects like birth defects or neurological damage.",
-      image: "http://www.esayap.com/images/Island1.jpg"
+      comment: "ZOMMMMBIESSSSS!!! RUNNNNNNNN!!!!!",
+      image: "http://image.dek-d.com/27/0283/4578/121404621"
     }
   ]
 }
