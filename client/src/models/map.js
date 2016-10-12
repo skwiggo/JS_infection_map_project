5bb683ec15f5dbbe252271cf6384d9b3da37517b
James Scott <jamessct@hotmail.co.uk>
2016-10-12 14:07:15 +0100

    mergergergergerge

--- client/src/models/map.js
+++ client/src/models/map.js
@@ -131,12 +131,14 @@ Map.prototype = {
   },
   getContentString: function(disease, country) {
     var i = this.getRandomFact(disease.facts);
-
-    var contentString = '<div id="infoWindowStyles">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<img id="infoWindowImage" src="' + disease.facts[i].image + '"/>' +
-      '<h3 id="heading">'+ disease.name + '</h3>'+
+    console.log(disease.facts)
+    var contentString = '<div id="infoWindowStyles">' +
+      '<div id="siteNotice">' +
+      '</div>' +
+      '<div id="headingDiv">' +
+      '<h3 id="heading">' + disease.name + '</h3>' +
+      '</div>' +
+      '<div id="subheadingDiv">' +
       '<h4 id="subHeading">' + country.name + '</h4>' +
       '<h4 id="subHeading">' + "Infection Rate: " + country.mortality.toUpperCase() + '</h4>' +
       '</div>' +
